23e1557c5aa8ba7aac78c033209142f53d49061a
Courtney Haltiwanger <chalt0319@gmail.com>
2018-08-14 14:24:46 +0000

    Can create new character

--- app/controllers/character_controller.rb
+++ app/controllers/character_controller.rb
@@ -1,5 +1,33 @@
 class CharacterController < ApplicationController
+
   get '/characters/index' do
-    erb :'/characters/index'
+    if logged_in?
+      @user = current_user
+      erb :'/characters/index'
+    else
+      redirect "/users/login"
+    end
   end
+
+  get '/characters/new' do
+    if logged_in?
+      erb :'/characters/new'
+    else
+      redirect "/users/login"
+    end
+  end
+
+  post '/characters/new' do
+    if logged_in?
+      @character = Character.find_or_create_by(params)
+      if !current_user.characters.include?(@character)
+        current_user.characters << @character
+        @character.save
+      end
+    else
+      redirect "/users/login"
+    end 
+  end
+
+
 end
