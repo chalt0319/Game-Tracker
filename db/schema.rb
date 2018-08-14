01629c5f2ee7a32162386f11be6ac1aae2fffeaa
Courtney Haltiwanger <chalt0319@gmail.com>
2018-08-14 20:06:06 +0000

    Removed ability column

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 3) do
+ActiveRecord::Schema.define(version: 4) do
 
   create_table "abilities", force: :cascade do |t|
     t.string "name"
@@ -20,7 +20,6 @@ ActiveRecord::Schema.define(version: 3) do
 
   create_table "characters", force: :cascade do |t|
     t.string "name"
-    t.string "ability"
     t.string "user_id"
   end
 
